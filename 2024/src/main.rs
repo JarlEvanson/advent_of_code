d6ae620847bebc507aa007abea0eaa620b5f92a0
Jarl Evanson <evanson.jarl@gmail.com>
2024-12-21 16:31:37 -0600

    2024-02: Solve

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -3,10 +3,11 @@ use std::process::ExitCode;
 use utils::Solution;
 
 mod day_01;
+mod day_02;
 
 pub const SOLUTIONS: [Option<fn(&str) -> Solution>; 25] = [
     Some(day_01::solve),
-    None,
+    Some(day_02::solve),
     None,
     None,
     None,
