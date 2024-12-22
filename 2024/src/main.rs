48ae2f68182a9022588390eafa642fa66bc5afe6
Jarl Evanson <evanson.jarl@gmail.com>
2024-12-21 19:58:49 -0600

    2024-05: Solve

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -8,13 +8,14 @@ pub mod day_01;
 pub mod day_02;
 pub mod day_03;
 pub mod day_04;
+pub mod day_05;
 
 pub const SOLUTIONS: [Option<fn(&str) -> Solution>; 25] = [
     Some(day_01::solve),
     Some(day_02::solve),
     Some(day_03::solve),
     Some(day_04::solve),
-    None,
+    Some(day_05::solve),
     None,
     None,
     None,
