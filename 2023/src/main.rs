edb466933dbfc8eb8822c7f6024082f169d28b08
Jarl Evanson <evanson.jarl@gmail.com>
2025-03-27 20:35:12 -0500

    2023-04: Solve

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -5,12 +5,13 @@ use utils::Solution;
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 pub const SOLUTIONS: [Option<fn(&str) -> Solution>; 25] = [
     Some(day_01::solve),
     Some(day_02::solve),
     Some(day_03::solve),
-    None,
+    Some(day_04::solve),
     None,
     None,
     None,
